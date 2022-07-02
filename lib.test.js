8b790b9df20085044a6125065c5aa8e7033466a5
t-ski <t.schiepanski@gmx.de>
2022-07-02 18:37:49 +0200

    Implement element class with concrete inline and block type extensions

--- lib.test.js
+++ lib.test.js
@@ -5,18 +5,26 @@ let testCounter = 1;
 
 
 function test(code, expectedTranslation) {
+    const unifyCode = code => {
+        return code.replace(/\s*(<)|(>)\s*/g, "$1$2");
+    };
+
     const actualTranslation = lib.translate(code);
 
-    if(actualTranslation === expectedTranslation) {
+    if(unifyCode(actualTranslation) === unifyCode(expectedTranslation)) {
         testCounter++;
 
         return;
     }
 
-    console.log(`Test ${testCounter} has failed:\n`);
-    console.log(`––– CODE:\n${code}`);
-    console.log(`––– EXPECTED:\n${expectedTranslation}`);
-    console.log(`––– ACTAUL:\n${actualTranslation}`);
+    const displayCode = (caption, code) => {
+        console.log(`\x1b[1m\x1b[33m––– ${caption}:\x1b[0m\x1b[2m\x1b[3m\x1b[37m\n${code}\x1b[0m`);
+    };
+
+    console.log(`\n\x1b[31mTest ${testCounter} has failed:\n`);
+    displayCode("CODE", code);
+    displayCode("EXPECTED", expectedTranslation);
+    displayCode("ACTAUL", actualTranslation);
 
     process.exit(1);
 }
@@ -24,14 +32,20 @@ function test(code, expectedTranslation) {
 
 test(`
 # Heading 1
-Text text text.
+
+Text Text Text.
 
 ## Heading 2
+
+--- awdwa
 `, `
-<h1>Title 1</h1>
-<p>Text Text Text</p>
+<h1>Heading 1</h1>
+<p>
+    Text Text Text.
+</p>
 <h2>Heading 2</h2>
+<hr>
 `);
 
 
-console.log("All tests have successfully passed.");
+console.log("\x1b[32mAll tests have successfully passed.");
